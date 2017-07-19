3447228f7ef456a467996f337db4142de26e8e2f
Cole Bemis <colebemis@gmail.com>
2017-07-19 14:03:57 -0700

    Set up React Styleguidist

---
+++ src/components/Hello.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Hello = props => <h1>{props.children}</h1>;
+
+export default Hello;
---
+++ styleguide.config.js
@@ -0,0 +1,11 @@
+const loaders = require('loaders');
+
+module.exports = {
+  components: 'src/components/**/[A-Z]*.js',
+  defaultExample: true,
+  webpackConfig: {
+    module: {
+      loaders: loaders.all,
+    },
+  },
+};
