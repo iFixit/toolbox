9e507375c39cea33bbc03aa9fd2b275c6dcfddb5
Cole Bemis <colebemis@gmail.com>
2017-07-19 22:08:16 -0700

    Use constants in Hello component

--- src/components/Hello/Hello.jsx
+++ src/components/Hello/Hello.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { H1 } from 'glamorous';
 
+import { color, fontSize } from '../../constants';
+
 /**
  * Hello Toolbox!
  */
 const Hello = props =>
-  <H1 color="blue">
+  <H1 color={color.blue[4]} fontSize={fontSize[2]}>
     Hello {props.message}
   </H1>;
 
