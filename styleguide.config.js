c40ffdff5f2b1d0da372faa544bf4ae4594f7496
Cole Bemis <colebemis@gmail.com>
2017-07-21 22:54:54 -0700

    Change component path line

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,10 +1,15 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const loaders = require('loaders');
+const path = require('path');
 
 module.exports = {
   components: 'src/components/**/*.jsx',
   showCode: true,
   showUsage: true,
+  getComponentPathLine(componentPath) {
+    const name = path.basename(componentPath, '.jsx');
+    return `import { ${name} } from 'toolbox';`;
+  },
   highlightTheme: 'duotone-light',
   theme: {
     color: {
