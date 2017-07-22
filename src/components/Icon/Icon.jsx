7e2a0d6e18ce1cdfb4d24fed3f1fbbf053b6ab7d
Cole Bemis <colebemis@gmail.com>
2017-07-21 22:14:07 -0700

    Add color, size and strokeWidth props

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -3,13 +3,50 @@ import PropTypes from 'prop-types';
 import glamorous from 'glamorous';
 import { toSvg } from 'feather-icons';
 
-const SvgContainer = glamorous.span();
+const SvgContainer = glamorous.span(
+  {
+    display: 'inline-block',
+  },
+  ({ size, color, strokeWidth }) => ({
+    width: size,
+    height: size,
+    color,
+    strokeWidth,
+  }),
+);
 
-const Icon = ({ name }) =>
-  <SvgContainer dangerouslySetInnerHTML={{ __html: toSvg(name) }} />;
+const Icon = props =>
+  <SvgContainer
+    size={props.size}
+    color={props.color}
+    strokeWidth={props.strokeWidth}
+    dangerouslySetInnerHTML={{
+      __html: toSvg(props.name, {
+        width: '100%',
+        height: '100%',
+        'stroke-width': 'inherit',
+      }),
+    }}
+  />;
 
 Icon.propTypes = {
+  /** Icon name */
   name: PropTypes.string.isRequired,
+
+  /** Any legal CSS color value for the stroke color */
+  color: PropTypes.string,
+
+  /** Width and height of the icon in pixels */
+  size: PropTypes.number,
+
+  /** Stroke width of the icon in pixels */
+  strokeWidth: PropTypes.number,
+};
+
+Icon.defaultProps = {
+  color: 'currentColor',
+  size: 24,
+  strokeWidth: 2,
 };
 
 export default Icon;
--- src/components/Icon/README.md
+++ src/components/Icon/README.md
@@ -1,3 +1,5 @@
+### Basic Usage
+
 ```
 <Icon name="home" />
 ```
