a55c03b7b86eb7a513d4e0e564c51a651828eb15
Cole Bemis <colebemis@gmail.com>
2017-07-28 13:59:50 -0700

    Export components

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as Icon } from './components/Icon/Icon';
+export { default as Tag } from './components/Tag/Tag';
+export { default as TextField } from './components/TextField/TextField';
