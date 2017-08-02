2ee9de44b204b8aa827ec7769d507f8b44d6f573
Cole Bemis <colebemis@gmail.com>
2017-08-02 13:44:53 -0700

    Add Button to export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+export { default as Button } from './components/Button/Button';
 export { default as Icon } from './components/Icon/Icon';
 export { default as Tag } from './components/Tag/Tag';
 export { default as TextField } from './components/TextField/TextField';
