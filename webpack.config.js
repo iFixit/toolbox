24e787d4ae002e8373b1157652732ec5799e15ad
Tyler Deitz <tylerdeitz@gmail.com>
2017-08-03 17:19:22 -0700

    fix externals

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,20 @@ module.exports = {
       ],
    },
    externals: {
-      react: 'React',
-      'react-dom': 'ReactDOM',
+      react: {
+         root: 'React',
+         commonjs2: 'react',
+         commonjs: 'react',
+         amd: 'react',
+         umd: 'react',
+      },
+      'react-dom': {
+         root: 'ReactDOM',
+         commonjs2: 'react-dom',
+         commonjs: 'react-dom',
+         amd: 'react-dom',
+         umd: 'react-dom',
+      },
    },
    resolve: {
       extensions: ['.js', '.jsx'],
