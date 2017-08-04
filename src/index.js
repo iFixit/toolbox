8a3783baf614376b9975553b3a1647a2a195fced
Tyler Deitz <tylerdeitz@gmail.com>
2017-08-04 14:46:42 -0700

    add state documentation

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import Button from './components/Button/Button';
 import Icon from './components/Icon/Icon';
 import Tag from './components/Tag/Tag';
 import TextField from './components/TextField/TextField';
+import Checkbox from './components/Checkbox/Checkbox';
+import Radio from './components/Radio/Radio';
 
 const toolbox = {};
 toolbox.Button = Button;
 toolbox.Icon = Icon;
 toolbox.Tag = Tag;
 toolbox.TextField = TextField;
+toolbox.Checkbox = Checkbox;
+toolbox.Radio = Radio;
 
 export default toolbox;
