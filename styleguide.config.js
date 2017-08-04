e628a78bd997feaf494091251faff45cedef3662
Tyler Deitz <tylerdeitz@gmail.com>
2017-08-04 11:38:02 -0700

    remove default option

--- styleguide.config.js
+++ styleguide.config.js
@@ -3,11 +3,6 @@ const path = require('path');
 
 module.exports = {
    title: 'Toolbox',
-   components: 'src/components/**/*.jsx',
-   getComponentPathLine(componentPath) {
-      const name = path.basename(componentPath, '.jsx');
-      return `import { ${name} } from 'toolbox'`;
-   },
    highlightTheme: 'elegant',
    theme: {
       color: {
