97ae520c8205448fcb34fc7e03a3fa57d9404c97
Tyler Deitz <tylerdeitz@gmail.com>
2017-08-03 18:57:22 -0700

    remove amd and umd

--- webpack.config.js
+++ webpack.config.js
@@ -25,18 +25,15 @@ module.exports = {
          root: 'React',
          commonjs2: 'react',
          commonjs: 'react',
-         amd: 'react',
-         umd: 'react',
       },
       'react-dom': {
          root: 'ReactDOM',
          commonjs2: 'react-dom',
          commonjs: 'react-dom',
-         amd: 'react-dom',
-         umd: 'react-dom',
       },
    },
    resolve: {
       extensions: ['.js', '.jsx'],
    },
+   devtool: 'cheap-module-eval-source-map',
 };
