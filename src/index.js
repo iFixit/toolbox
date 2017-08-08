9356772302f21a509c60103b59c651e3f0caf9fd
Cole Bemis <colebemis@gmail.com>
2017-08-07 17:11:51 -0700

    Update toolbox export

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import Button from './components/Button/Button';
 import Checkbox from './components/Checkbox/Checkbox';
-import CheckboxRadio from './components/CheckboxRadio/CheckboxRadio';
 import Icon from './components/Icon/Icon';
 import Radio from './components/Radio/Radio';
-import Tag from './components/Tag/Tag';
+import RadioGroup from './components/RadioGroup/RadioGroup';
+import Textarea from './components/Textarea/Textarea';
 import TextField from './components/TextField/TextField';
 
 const toolbox = {
    Button,
-   CheckboxRadio,
    Checkbox,
    Icon,
    Radio,
-   Tag,
+   RadioGroup,
+   Textarea,
    TextField,
 };
 
