859f9b6b636b212fda16e8a84952b770f503c774
Cole Bemis <colebemis@gmail.com>
2017-08-11 12:19:22 -0700

    Used named exports

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
-import Button from './components/Button/Button';
-import Checkbox from './components/Checkbox/Checkbox';
-import Icon from './components/Icon/Icon';
-import Radio from './components/Radio/Radio';
-import RadioGroup from './components/RadioGroup/RadioGroup';
-import Textarea from './components/Textarea/Textarea';
-import TextField from './components/TextField/TextField';
-
-const toolbox = {
-   Button,
-   Checkbox,
-   Icon,
-   Radio,
-   RadioGroup,
-   Textarea,
-   TextField,
-};
-
-export default toolbox;
+export { default as Button } from './components/Button/Button';
+export { default as Checkbox } from './components/Checkbox/Checkbox';
+export { default as Icon } from './components/Icon/Icon';
+export { default as Radio } from './components/Radio/Radio';
+export { default as RadioGroup } from './components/RadioGroup/RadioGroup';
+export { default as Textarea } from './components/Textarea/Textarea';
+export { default as TextField } from './components/TextField/TextField';
