7141de8d0d554700935d2603a7d411ca19256ff6
Cole Bemis <colebemis@gmail.com>
2017-08-18 12:00:22 -0700

    Export Grid component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './components/Button/Button';
 export { default as Checkbox } from './components/Checkbox/Checkbox';
+export { default as Grid } from './components/Grid/Grid';
 export { default as Icon } from './components/Icon/Icon';
 export { default as Radio } from './components/Radio/Radio';
 export { default as RadioGroup } from './components/RadioGroup/RadioGroup';
