da05cac1f807621200ac2a3ec191fe9ee51bea70
Cole Bemis <colebemis@gmail.com>
2017-08-19 01:29:01 -0700

    Export PageNavBar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export { default as Button } from './components/Button/Button';
 export { default as Checkbox } from './components/Checkbox/Checkbox';
 export { default as Grid } from './components/Grid/Grid';
 export { default as Icon } from './components/Icon/Icon';
+export { default as PageNavBar } from './components/PageNavBar/PageNavBar';
 export { default as Radio } from './components/Radio/Radio';
 export { default as RadioGroup } from './components/RadioGroup/RadioGroup';
 export { default as Textarea } from './components/Textarea/Textarea';
