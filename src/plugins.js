a9d2ac6e2ebc381689ed92212098f7cbb1b42761
Cole Bemis <colebemis@gmail.com>
2017-08-21 16:28:50 -0700

    Add comment explaining purpose of plugin

--- src/plugins.js
+++ src/plugins.js
@@ -1,5 +1,9 @@
 import { plugins } from 'glamor';
 
+/**
+ * This prefixes `glamor` css selectors with `[data-reactroot]`
+ * so that global styles don't override `glamor` styles.
+ */
 function specificityPlugin({ selector, style }) {
    const prefixedSelector = selector
       .split(',')
