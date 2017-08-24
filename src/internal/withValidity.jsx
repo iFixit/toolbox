71a0e635d9ef2c323c44c64140b083612cedddc6
Tyler Deitz <tylerdeitz@gmail.com>
2017-08-24 10:05:28 -0700

    correctly detructure validity object

--- src/internal/withValidity.jsx
+++ src/internal/withValidity.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const getValidity = ({
    validationMessage = '',
-   validity: valid = true,
+   validity: { valid } = true,
 } = {}) => ({
    valid,
    validationMessage,
