56bb78d7d6d90d53c6beb7c4de3af7c955ddbe7e
Tyler Deitz <tylerdeitz@gmail.com>
2017-08-28 10:13:32 -0700

    comsolidate textarea and textfield component

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -1,100 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import glamorous from 'glamorous';
 
-import constants from '../../constants';
-import domProps from '../../internal/domProps';
+import TextareaTextField from '../../internal/TextareaTextField';
 
-const { borderRadius, color, fontSize, lineHeight, spacing } = constants;
-
-const Label = glamorous.label({
-   display: 'inline-block',
-   boxSizing: 'border-box',
-   width: '100%',
-   fontSize: fontSize[2],
-   color: color.grayAlpha[9],
-});
-
-const LabelText = glamorous.span({
-   display: 'inline-block',
-   marginBottom: spacing[3],
-   lineHeight: lineHeight.solid,
-});
-
-const Input = glamorous('input', { forwardProps: 'onInvalid' })(
-   {
-      display: 'block',
-      width: '100%',
-      boxSizing: 'border-box',
-      padding: `${spacing[1]} ${spacing[3]}`,
-      fontFamily: 'inherit',
-      fontSize: fontSize[2],
-      lineHeight: lineHeight.copy,
-      border: `1px solid ${color.grayAlpha[3]}`,
-      borderRadius,
-      outline: 'none',
-      color: color.grayAlpha[9],
-      backgroundColor: color.white,
-   },
-   ({ domProps: { focus } }) => focus && {
-      borderColor: color.blue[4],
-      boxShadow: `0 0 0 1px ${color.blue[4]}`,
-   },
-   ({ showInvalid }) => showInvalid && {
-      color: color.red[4],
-      backgroundColor: color.red[1],
-   },
-   ({ disabled }) => disabled && {
-      color: color.grayAlpha[5],
-      backgroundColor: color.grayAlpha[1],
-   },
+const TextField = props => (
+   <TextareaTextField component="TextField" {...props} />
 );
-
-class TextField extends React.Component {
-   constructor(props) {
-      super(props);
-      this.state = { showInvalid: false };
-   }
-
-   render() {
-      const {
-         className,
-         label,
-         onMouseEnter,
-         onMouseLeave,
-         onChange,
-         showInvalid,
-         ...props
-      } = this.props || this.state;
-
-      return (
-         <Label
-            className={className}
-            onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}
-         >
-            {label !== '' &&
-               <LabelText> {label} </LabelText>
-            }
-            {showInvalid &&
-               <LabelText> {props.domProps.validationMessage} </LabelText>
-            }
-            <domProps.Target>
-               <Input
-                  {...props}
-                  {...{ showInvalid }}
-                  onInvalid={ev => {
-                     ev.preventDefault();
-                     this.setState({ showInvalid: true });
-                  }}
-                  onChange={ev => onChange({ value: ev.target.value })}
-                  innerRef={props.domProps.setRef}
-               />
-            </domProps.Target>
-         </Label>
-      );
-   }
-}
 
 TextField.propTypes = {
    /** Set class name of containing element. */
@@ -151,4 +62,4 @@ TextField.defaultProps = {
    onMouseLeave: () => {},
 };
 
-export default domProps(TextField);
+export default TextField;
--- src/components/Textarea/Textarea.jsx
+++ src/components/Textarea/Textarea.jsx
@@ -1,103 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import glamorous from 'glamorous';
 
-import constants from '../../constants';
-import domProps from '../../internal/domProps';
+import TextareaTextField from '../../internal/TextareaTextField';
 
-const { borderRadius, color, fontSize, lineHeight, spacing } = constants;
-
-const Label = glamorous.label({
-   display: 'inline-block',
-   width: '100%',
-   fontSize: fontSize[2],
-   color: color.grayAlpha[9],
-});
-
-const LabelText = glamorous.span({
-   display: 'inline-block',
-   marginBottom: spacing[3],
-   lineHeight: lineHeight.solid,
-});
-
-const TextareaContainer = glamorous('textarea', { forwardProps: 'onInvalid' })(
-   {
-      boxSizing: 'border-box',
-      width: '100%',
-      margin: 0,
-      padding: `${spacing[2]} ${spacing[3]}`,
-      fontFamily: 'inherit',
-      fontSize: fontSize[2],
-      lineHeight: lineHeight.copy,
-      border: `1px solid ${color.grayAlpha[3]}`,
-      borderRadius,
-      boxShadow: 'none',
-      outline: 'none',
-      color: color.grayAlpha[9],
-      backgroundColor: color.white,
-   },
-   ({ domProps: { focus } }) => focus && {
-      borderColor: color.blue[4],
-      boxShadow: `0 0 0 1px ${color.blue[4]}`,
-   },
-   ({ resize }) => ({
-      resize,
-   }),
-   ({ showInvalid }) => showInvalid && {
-      color: color.red[4],
-      backgroundColor: color.red[1],
-   },
-   ({ disabled }) => disabled && {
-      color: color.grayAlpha[5],
-      backgroundColor: color.grayAlpha[1],
-   },
+const Textarea = props => (
+   <TextareaTextField component="TextArea" {...props} />
 );
-
-class Textarea extends React.Component {
-   constructor(props) {
-      super(props);
-      this.state = { showInvalid: false };
-   }
-
-   render() {
-      const {
-         className,
-         label,
-         onMouseEnter,
-         onMouseLeave,
-         onChange,
-         showInvalid,
-         ...props
-      } = this.props || this.state;
-
-      return (
-         <Label
-            className={className}
-            onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}
-         >
-            {label !== '' &&
-               <LabelText> {label} </LabelText>
-            }
-            {showInvalid &&
-               <LabelText> {props.domProps.validationMessage} </LabelText>
-            }
-            <domProps.Target>
-               <TextareaContainer
-                  {...props}
-                  {...{ showInvalid }}
-                  onChange={ev => onChange({ value: ev.target.value })}
-                  onInvalid={ev => {
-                     ev.preventDefault();
-                     this.setState({ showInvalid: true });
-                  }}
-                  innerRef={props.domProps.setRef}
-               />
-            </domProps.Target>
-         </Label>
-      );
-   }
-}
 
 Textarea.propTypes = {
    /** Set class name of containing element. */
@@ -144,4 +52,4 @@ Textarea.defaultProps = {
    onMouseLeave: () => {},
 };
 
-export default domProps(Textarea);
+export default Textarea;
