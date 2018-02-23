52610a2564ee47ee4b83c173df545c1753eb3962
Cole Bemis <colebemis@gmail.com>
2018-02-23 13:18:41 -0800

    Add Label component

---
+++ src/components/Label/Label.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { string } from 'prop-types';
+import glamorous from 'glamorous';
+import constants from '../../constants';
+
+const { spacing, fontSize, lineHeight, color, borderRadius } = constants;
+
+const LabelContainer = glamorous.span(props => ({
+   display: 'inline-block',
+   padding: `${spacing[0]} ${spacing[1]}`,
+   fontSize: fontSize[0],
+   fontWeight: 700,
+   lineHeight: lineHeight.title,
+   textTransform: 'uppercase',
+   color: props.color,
+   backgroundColor: props.background,
+   borderRadius,
+   boxShadow: `inset 0 0 0 1px ${color.grayAlpha[3]}`,
+}));
+
+const Label = props => (
+   <LabelContainer {...props} />
+);
+
+Label.propTypes = {
+   /** Set the label background color. */
+   background: string,
+   /** Set the label text color. */
+   color: string,
+};
+
+Label.defaultProps = {
+   background: color.blue[4],
+   color: color.white,
+};
+
+export default Label;
---
+++ src/components/Label/README.md
@@ -0,0 +1,17 @@
+### Examples
+
+#### Simple Label
+
+```
+<Label>Featured</Label>
+```
+
+#### Alternate colors
+
+The `<Label>` component exposes `color` and `background` props allowing you to customize the component to your specific use case. Remember to use the colors defined in the `constants` object instead of hard-coding HEX or RGBA values.
+
+```
+// NOTE: These colors are hard-coded for demo purposes only. Don't hard-code HEX values in production code.
+
+<Label color="#C27E00" background="#FEF5D9">Pending</Label>
+```
