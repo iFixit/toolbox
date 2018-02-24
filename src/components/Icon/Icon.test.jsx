165294a737c372d86740e2a0b324755ff9146154
Cole Bemis <colebemis@gmail.com>
2018-02-24 08:57:55 -0800

    Test Icon props

--- src/components/Icon/Icon.test.jsx
+++ src/components/Icon/Icon.test.jsx
@@ -6,3 +6,10 @@ test('renders without crashing', () => {
    const tree = renderer.create(<Icon name="square" />).toJSON();
    expect(tree).toMatchSnapshot();
 });
+
+test('renders with custom color, size and stroke width', () => {
+   const tree = renderer
+      .create(<Icon name="home" color="#f00" size={16} strokeWidth={1} />)
+      .toJSON();
+   expect(tree).toMatchSnapshot();
+});
