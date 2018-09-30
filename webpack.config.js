7906e5402f9f5b46b16a1cfaef469da7371066fe
Cole Bemis <colebemis@gmail.com>
2018-09-30 13:58:51 -0700

    fix: Update to babel@7 and webpack@4

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
    entry: { toolbox: './src/index.js' },
@@ -10,15 +9,11 @@ module.exports = {
       filename: '[name].js',
    },
    module: {
-      loaders: [
+      rules: [
          {
             test: /\.(js|jsx)$/,
             exclude: /node_modules/,
             loader: 'babel-loader',
-            query: {
-               presets: ['env', 'react-app'],
-               compact: true,
-            },
          },
       ],
    },
@@ -45,5 +40,4 @@ module.exports = {
    resolve: {
       extensions: ['.js', '.jsx'],
    },
-   plugins: [new webpack.optimize.UglifyJsPlugin()],
 };
