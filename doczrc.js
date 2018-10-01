7e641893720347c8383b81ff74ec09757492463d
Cole Bemis <colebemis@gmail.com>
2018-10-01 14:33:29 -0700

    docs: Add docz pages

---
+++ doczrc.js
@@ -0,0 +1,13 @@
+export default {
+   themeConfig: {
+      styles: {
+         h1: {
+            fontWeight: 600,
+         },
+         table: {
+            fontFamily: '"Source Sans Pro"',
+            fontSize: 16,
+         },
+      },
+   },
+};
